8ff924324520f718cf80f438be43534fa7805c4a
Hrithik <hrithiknonia66@gmail.com>
2026-08-10 14:00:38 +0530

    profile page and auth page ke beech toggle complete

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,3 +1,41 @@
+// built in imports
+
+// custom imports
+import ProfileHeader from "../components/layout/ProfileHeader";
+import UserStatsBar from "../components/layout/UserStatsBar";
+import AccountSettings from "../components/layout/AccountSettings";
+import RecentOrders from "../components/layout/RecentOrders";
+
 export default function Profile() {
-  return <>hello</>;
+  return (
+    <>
+      <section className="bg-slate-50 p-6 space-y-7">
+        {/* profile name */}
+        <div>
+          <ProfileHeader />
+        </div>
+
+        {/* main content */}
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+          {/* setting options */}
+          <div className="lg:col-span-3 space-y-5">
+            {/* info badge */}
+            <div>
+              <UserStatsBar />
+            </div>
+
+            {/* setting options */}
+            <div>
+              <AccountSettings />
+            </div>
+          </div>
+
+          {/* setting data */}
+          <div className="lg:col-span-1">
+            <RecentOrders />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,48 +1,13 @@
 // built in imports
 
 // custom imports
-import ProfileHeader from "../components/layout/ProfileHeader";
-import RecentOrders from "../components/layout/RecentOrders";
-import UserStatsBar from "../components/layout/UserStatsBar";
-import AccountSettings from "../components/layout/AccountSettings";
 import AuthPage from "../components/auth/AuthPage";
 
 function ProfilePage() {
-  const showFormBtn = true;
   return (
     <>
       <section className="bg-slate-50 p-6 space-y-7">
-        {showFormBtn ? (
-          <AuthPage />
-        ) : (
-          <>
-            {/* profile name */}
-            <div>
-              <ProfileHeader />
-            </div>
-
-            {/* main content */}
-            <div className="grid grid-cols-1 lg:grid-cols-4 gap-5">
-              {/* setting options */}
-              <div className="lg:col-span-3 space-y-5">
-                {/* info badge */}
-                <div>
-                  <UserStatsBar />
-                </div>
-
-                {/* setting options */}
-                <div>
-                  <AccountSettings />
-                </div>
-              </div>
-
-              {/* setting data */}
-              <div className="lg:col-span-1">
-                <RecentOrders />
-              </div>
-            </div>
-          </>
-        )}
+        <AuthPage />
       </section>
     </>
   );
